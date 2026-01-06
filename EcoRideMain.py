9909c1cfc543af03535a1b50234f1df3ab8c67a6
ENUKOLUnaga <enkolunagendra@gmail.com>
2026-01-06 11:44:51 +0530

    Initial commit

---
+++ EcoRideMain.py
@@ -0,0 +1,11 @@
+# UC 1 Basic Fleet setup
+class Vehicle:
+    def __init__(self,vehicle_id,model,battery_percentage):
+        self.vehicle_id = vehicle_id
+        self.model = model
+        self.battery_percentage = battery_percentage
+
+def main():
+    print("Welcome to EcoRide Urban Mobility")
+if __name__ == "__main__":
+    main()
