691fbac67df65246f9a8cfe729a8d9a796882a0e
ENUKOLUnaga <enkolunagendra@gmail.com>
2026-01-07 10:49:20 +0530

    Initial commit - Eco-Ride system setup

--- EcoRideMain.py
+++ EcoRideMain.py
@@ -1,49 +1,6 @@
-from abc import ABC, abstractmethod
-# UC 1,2: Vehicle Base Class
-class Vehicle(ABC):
-    def __init__(self,vehicle_id,model,battery_percentage):
-        self.vehicle_id = vehicle_id
-        self.model = model
-        self.battery_percentage = battery_percentage
-        self.__maintenance_status = "Available"
-        self.__rental_price=0.0
+from Vehicle import Vehicle
 
-    # Encapsulation: Getters & Setters
-    @property
-    def maintenance_status(self):
-        return self.__maintenance_status
-    @maintenance_status.setter
-    def maintenance_status(self, status):
-        if 0 < self.battery_percentage <= 100:
-            self.__maintenance_status = status
-    @property
-    def rental_price(self):
-        return self.__rental_price
-    @rental_price.setter
-    def rental_price(self, rental_price):
-        if self.rental_price>=0:
-            self.__rental_price = rental_price
-    # UC 4: Abstraction
-    @abstractmethod
-    def calculate_trip_cost(self, value):
-        pass
+print("Welcome to Eco-Ride Urban Mobility System")
 
-#UC-3 Inheritance And Specialisation
-class ElectricCar(Vehicle):
-    def __init__(self, vehicle_id, model, battery_percentage, seating_capacity):
-        super().__init__(vehicle_id, model, battery_percentage)
-        self.seating_capacity = seating_capacity
-    def calculate_trip_cost(self, distance_km):
-        return 5.0 + (0.5 * distance_km)
-
-class ElectricScooter(Vehicle):
-    def __init__(self, vehicle_id, model, battery_percentage, max_speed_limit):
-        super().__init__(vehicle_id, model, battery_percentage)
-        self.max_speed_limit = max_speed_limit
-    def calculate_trip_cost(self, minutes):
-        return 1.0 + (0.15 * minutes)
-    
-def main():
-    print("Welcome to Eco-Rider Urban Mobility System")
-if __name__ == "__main__":
-    main()
+v1 = Vehicle("V001", "Tesla Model 3", 90)
+print(v1.model, v1.battery_percentage)
