1d7b9f3b7467537ce4fdcd75f7d4bab27288a0aa
ENUKOLUnaga <enkolunagendra@gmail.com>
2026-01-07 15:41:30 +0530

    UC3-Inheritance

--- EcoRideMain.py
+++ EcoRideMain.py
@@ -1,5 +1,9 @@
-from src.Vehicle import Vehicle
+from src.electric_car import ElectricCar
+from src.electric_scooter import ElectricScooter
 
 print("Welcome to Eco-Ride Urban Mobility System")
-v1 = Vehicle("V001", "Tesla Model 3", 90)
-print(v1.model, v1.battery_percentage)
+#vehicle creation
+car1 = ElectricCar("C101", "Tesla Model 3", 90, 5)
+scooter1 = ElectricScooter("S201", "Xiaomi Pro", 85, 25)
+print(car1.model, car1.battery_percentage)
+print(scooter1.model, scooter1.battery_percentage)
